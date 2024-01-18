b6a88a437bcaa6ea776235528b17ebe24c8eff69
charvirus <jbrandon413@naver.com>
2024-01-18 16:32:00 +0900

    11650 Comparator 공부해야겠다

--- src/BookMark/boj11650.java
+++ src/BookMark/boj11650.java
@@ -1,10 +1,12 @@
+package BookMark;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class boj11650 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -33,5 +35,4 @@ public class Main {
 
         System.out.println(sb);
     }
-
 }
