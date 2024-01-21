ef59043db22a853985081c23fafcb20fedf138c4
BrandonJ <jbrandon413@naver.com>
2024-01-22 01:21:20 +0900

    계속 공부하자...

---
+++ src/BookMark/boj18870.java
@@ -0,0 +1,43 @@
+package BookMark;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class boj18870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int inp = Integer.parseInt(br.readLine());
+        int[] arr = new int[inp];
+        int[] sortArr = new int[inp];
+        HashMap<Integer, Integer> integerHashMap = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+
+        for (int i = 0; i < inp; i++) {
+            sortArr[i] = arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(sortArr);
+
+        int rnk = 0;
+        for (int i : sortArr) {
+            if (!integerHashMap.containsKey(i)) {
+                integerHashMap.put(i, rnk);
+                rnk++;
+            }
+        }
+
+        for (int k : arr) {
+            int rk = integerHashMap.get(k);
+            sb.append(rk).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ src/BookMark/boj2839.java
@@ -0,0 +1,23 @@
+package BookMark;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj2839 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int inp = Integer.parseInt(br.readLine());
+
+        if (inp == 4 || inp == 7) {
+            System.out.println(-1);
+        } else if (inp % 5 == 0) {
+            System.out.println(inp / 5);
+        } else if (inp % 5 == 1 || inp % 5 == 3) {
+            System.out.println(inp / 5 + 1);
+        } else if (inp % 5 == 4 || inp % 5 == 2) {
+            System.out.println(inp / 5 + 2);
+        }
+    }
+}
