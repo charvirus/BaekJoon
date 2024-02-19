3108fd75cc31d0048c49a2747bbea4e5bf2ca257
BrandonJ <jbrandon413@naver.com>
2024-02-19 22:19:34 +0900

    불망초심....

--- src/class2/boj18111.java
+++ src/class2/boj18111.java
@@ -1,9 +1,11 @@
+package class2;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class boj18111 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
