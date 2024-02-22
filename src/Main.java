8c4c2f10c1998c7f445723c2e7b6f1d1d63063c4
charvirus <jbrandon413@naver.com>
2024-02-22 17:58:58 +0900

    11723 추가

--- src/Main.java
+++ src/Main.java
@@ -6,53 +6,9 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int[][] ground = new int[N][M];
+        int cnt = Integer.parseInt(br.readLine());
 
-        int min = 256;
-        int max = 0;
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                ground[i][j] = Integer.parseInt(st.nextToken());
-                if (ground[i][j] < min) {
-                    min = ground[i][j];
-                }
-                if (ground[i][j] > max) {
-                    max = ground[i][j];
-                }
-            }
-        }
-
-        int min_time = 1000000000;
-        int max_height = 0;
-
-        for (int height = min; height <= max; height++) {
-            int count = 0;
-            int block = B;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (ground[i][j] < height) {
-                        count += height - ground[i][j];
-                        block -= height - ground[i][j];
-                    } else {
-                        count += (ground[i][j] - height) * 2;
-                        block += ground[i][j] - height;
-                    }
-                }
-            }
-            if (block < 0){
-                break;
-            }
-            if(min_time >= count){
-                min_time = count;
-                max_height = height;
-            }
-        }
-        System.out.println(min_time+" "+max_height);
     }
 }
