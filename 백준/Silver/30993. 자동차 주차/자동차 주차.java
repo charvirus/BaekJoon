90facd27cbb77f631fd0d697a6b20955a13c1cc8
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-03-29 11:54:14 +0900

    [Silver V] Title: 자동차 주차, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/30993. 자동차 주차/자동차 주차.java
+++ 백준/Silver/30993. 자동차 주차/자동차 주차.java
@@ -14,6 +14,11 @@ public class Main {
         System.out.println(fact(N) / (fact(A) * fact(B) * fact(C)));
     }
 
+    /*N개의 차들을 색깔에 상관없이 나열하면 경우의 수는 N!
+    A개의 차들은 색깔이 같으므로 다른애들은 고정시키고 얘네끼리만 바뀌면 똑같으므로 A!로 나누고
+    B개의 차들은 색깔이 같으므로 다른애들은 고정시키고 얘네끼리만 바뀌면 똑같으므로 B!로 나누고
+    C개의 차들은 색깔이 같으므로 다른애들은 고정시키고 얘네끼리만 바뀌면 똑같으므로 C!로 나누면
+    결과는 N!/A!B!C!*/
     static long fact(long n) {
         if (n <= 1) {
             return 1;
