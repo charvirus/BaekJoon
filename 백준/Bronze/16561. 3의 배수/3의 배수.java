1dadedca7aaa1e432d4a99df0e32a20fc2818df8
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-04-15 11:28:40 +0900

    [Bronze II] Title: 3의 배수, Time: 128 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/16561. 3의 배수/3의 배수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int div = n / 3 - 2;
+
+        int result = 0;
+
+        for (int i = 1; i <= div; i++) {
+            result += i;
+        }
+
+        System.out.println(result);
+    }
+}
