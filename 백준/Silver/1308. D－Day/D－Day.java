af400be6b2621a23c64b17c1c21c35e8afaebbc9
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-04-17 15:48:53 +0900

    [Silver V] Title: D-Day, Time: 144 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/1308. D－Day/D－Day.java
+++ 백준/Silver/1308. D－Day/D－Day.java
@@ -1,44 +1,55 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
-        int y1 = sc.nextInt();
-        int m1 = sc.nextInt();
-        int d1 = sc.nextInt();
-        int y2 = sc.nextInt();
-        int m2 = sc.nextInt();
-        int d2 = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int setY = Integer.parseInt(st.nextToken());
+        int setM = Integer.parseInt(st.nextToken());
+        int setD = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        int ddayY = Integer.parseInt(st.nextToken());
+        int ddayM = Integer.parseInt(st.nextToken());
+        int ddayD = Integer.parseInt(st.nextToken());
 
-        int total1 = calcDay(y1, m1, d1);
-        int total2 = calcDay(y2, m2, d2);
 
-        if(y2 - y1 > 1000 || y2 - y1 == 1000 && (m1 < m2 || m1 == m2 && d1 <= d2)) System.out.println("gg");
-        else System.out.println("D-" + (total2 - total1));
+        if (ddayY - setY > 1000 || ddayY - setY == 1000 && (setM < ddayM || setM == ddayM && setD <= ddayD)) {
+            System.out.println("gg");
+        } else {
+            System.out.println("D-" + (calcutaleDay(ddayY, ddayM, ddayD) - calcutaleDay(setY, setM, setD)));
+        }
+
     }
 
-    static int calcDay(int y, int m, int d){
-        int[] day = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    static int calcutaleDay(int y, int m, int d) {
+        int[] monthDay = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         int days = 0;
 
-        for(int i = 1; i < y; i++){
-            days += 365 + checkYear(i);
+        for (int i = 1; i < y; i++) {
+            days += 365 + isLeapYear(i);
         }
 
-        for(int i = 1; i < m; i++){
-            if(i == 2) days += checkYear(y);
-            days += day[i];
+        for (int i = 1; i < m; i++) {
+            if (i == 2) {
+                days += isLeapYear(y);
+            }
+            days += monthDay[i];
         }
-
         days += d;
 
         return days;
     }
 
-    static int checkYear(int y){
-        if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) return 1;
-        else return 0;
+    static int isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
