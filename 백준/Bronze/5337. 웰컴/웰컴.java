78ced8f02226e9720ac7cc069111a3ec221b7dd7
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-05-05 14:47:19 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
