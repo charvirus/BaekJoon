1710968fc2c793d87d7f16431a93ee5238e9b015
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-05-23 22:19:58 +0900

    [Silver V] Title: 돌 게임, Time: 132 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int inp = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[1001];
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 1;
+        for(int i = 4;i<=inp;i++){
+            arr[i] = Math.min(arr[i-1],arr[i-3])+1;
+        }
+        if(arr[inp] % 2==0){
+            System.out.println("CY");
+        }else {
+            System.out.println("SK");
+        }
+    }
+}
