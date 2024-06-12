211f0f9ec0edc4c9b6fb81f91a2f20ac76ab70bd
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-06-12 09:35:00 +0900

    [Platinum V] Title: 약 팔기, Time: 172 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("2000");
+        for (int i = 0; i < 1000; i++) {
+            System.out.print(1+" ");
+        }for (int i = 0; i < 1000; i++) {
+            System.out.print(1000+" ");
+        }
+    }
+}
