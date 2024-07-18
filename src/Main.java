0b47f385aab3c5dd692fa2726d0506105a4b02c2
charvirus <jbrandon413@naver.com>
2024-07-18 13:56:36 +0900

    no message

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int max = 0;
-    static int totalcnt = 0;
-    static int y;
-    static int x;
-
+    static long fibo[] = new long[100];
     public static void main(String[] args) throws IOException {
+        setFibo();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int cnt = 0;
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
 
-        y = Integer.parseInt(st.nextToken());
-        x = Integer.parseInt(st.nextToken());
-        String[][] arr = new String[y][x];
-
-        for (int i = 0; i < y; i++) {
+        for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < x; j++) {
-                arr[i][j] = st.nextToken();
-            }
+            int k = Integer.parseInt(st.nextToken());
+            long x = Long.parseLong(st.nextToken());
+            System.out.println(checkFibo(k, x) ? "YES" : "NO");
         }
-
-        checkXY(arr);
-
-        System.out.println(totalcnt - max);
     }
 
-    static void checkXY(String[][] arr) {
-        for (int i = 0; i < y; i++) {
-            int cnt = 0;
-            for (int j = 0; j < x; j++) {
-                for (int k = 0; k < arr[i][j].length(); k++) {
-                    if (arr[i][j].charAt(k) == '9') {
-                        cnt++;
-                        totalcnt++;
+    static boolean checkFibo(int k, long x) {
+        if (k == 1) {
+            for (int i = 1; i < 100; i++) {
+                if (x == fibo[i]) {
+                    return true;
+                }
+            }
+        } else if (k == 2) {
+            for (int i = 1; i < 100; i++) {
+                for (int j = 1; j < 100; j++) {
+                    if (x == fibo[i] + fibo[j]) {
+                        return true;
                     }
                 }
             }
-            if (cnt > max) {
-                max = cnt;
-            }
-        }
-
-        for (int i = 0; i < x; i++) {
-            int cnt = 0;
-            for (int j = 0; j < y; j++) {
-                for (int k = 0; k < arr[j][i].length(); k++) {
-                    if (arr[j][i].charAt(k) == '9') {
-                        cnt++;
+        } else {
+            for (int i = 1; i < 100; i++) {
+                for (int j = 1; j < 100; j++) {
+                    for (int l = 1; l < 100; l++) {
+                        if (x == fibo[i] + fibo[j] + fibo[l]) {
+                            return true;
+                        }
                     }
                 }
             }
-            if (cnt > max) {
-                max = cnt;
-            }
         }
+        return false;
+    }
 
-
+    static void setFibo() {
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for (int i = 2; i < 100; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
     }
 }
