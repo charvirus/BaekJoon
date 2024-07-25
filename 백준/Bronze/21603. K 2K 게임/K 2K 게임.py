e12d03d94fb79a5f70771bdf8f72cdda093aa871
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-07-25 22:39:57 +0900

    [Bronze III] Title: K 2K 게임, Time: 88 ms, Memory: 35108 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
+++ 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
@@ -1,10 +1,8 @@
 N, K = map(int, input().split())
 numlist = []
-cnt = 0
 for i in range(1, N + 1):
-    if not (K % 10 == i % 10 or 2 * (K % 10) == i % 10):
-        cnt += 1
+    if not (K % 10 == i % 10 or (2 * K) % 10 == i % 10):
         numlist.append(i)
 
-print(cnt)
+print(len(numlist))
 print(*numlist)
