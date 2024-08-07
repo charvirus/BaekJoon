791b951da8b2a9b78af435e97d52a2318df517d1
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-08-07 11:55:16 +0900

    [Bronze III] Title: 수찬은 마린보이야!!, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+if N == 0:
+    print("divide by zero")
+    sys.exit(0)
+
+practice = list(map(int, sys.stdin.readline().split()))
+
+print("1.00")
+
