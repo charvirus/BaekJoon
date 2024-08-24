ee64239bb18f62358b4dbbb85ba468172735a40a
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-08-24 21:24:13 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static Integer[][] dp = new Integer[41][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            fibonacci(N);
+            sb.append(dp[N][0]).append(" ").append(dp[N][1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static Integer[] fibonacci(int num) {
+        if (dp[num][0] == null || dp[num][1] == null) {
+            dp[num][0] = fibonacci(num - 1)[0] + fibonacci(num - 2)[0];
+            dp[num][1] = fibonacci(num - 1)[1] + fibonacci(num - 2)[1];
+        }
+        return dp[num];
+    }
+}
