d17c70eec69119590cbf4ea6e0f22b82d0fa0c66
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-09-11 11:52:26 +0900

    [Bronze III] Title: 수학적 호기심, Time: 240 ms, Memory: 111332 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,4 +1,3 @@
-import math
 import sys
 
 input = sys.stdin.readline
@@ -8,9 +7,9 @@ n = int(input())
 for i in range(n):
     n, m = map(int, input().split())
     cnt = 0
-    for a in range(1, n+1):
-        for b in range(a+1,n):
-            if (a ** 2 + b ** 2 + m) % (a * b) == 0:
+    for a in range(1, n + 1):
+        for b in range(a + 1, n):
+            if ((a ** 2 + b ** 2 + m) / (a * b)).is_integer():
                 cnt += 1
 
     print(cnt)
