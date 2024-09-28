517346c71375ad2a1f72473d1c2ebd508291fddb
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-09-28 12:42:02 +0900

    [Silver II] Title: K번째 소수, Time: 392 ms, Memory: 203824 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -7,9 +7,9 @@ M = 7400000
 net = [False for i in range(M + 1)]
 
 for i in range(2, int(M ** 0.5) + 1):
-    if net[i] == False:
+    if not net[i]:
         for j in range(i*i,M+1,i):
             net[j] = True
 
-arr = [i for i in range(2,M+1) if net[i]==False]
+arr = [i for i in range(2,M+1) if not net[i]]
 print(arr[N-1])
