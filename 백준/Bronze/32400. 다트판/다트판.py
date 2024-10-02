f517c39edd73990bc8977c864a8c2ba63a85b2a8
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-10-02 16:49:57 +0900

    [Bronze II] Title: 다트판, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/32400. 다트판/다트판.py
+++ 백준/Bronze/32400. 다트판/다트판.py
@@ -2,12 +2,8 @@ import sys
 
 input = sys.stdin.readline
 
-dart = list(map(int, input().split()))
-Alice = 0
-twentIdx = dart.index(20)
-if twentIdx == 19:
-    Alice =(20 + dart[0] + dart[twentIdx - 1]) / 3
-else:
-    Alice = (20 + dart[twentIdx + 1] + dart[twentIdx - 1]) / 3
-Bob = sum(dart) / len(dart)
-print("Alice" if Alice > Bob else "Tie" if Alice == Bob else "Bob")
+d = [*map(int, input().split())]
+i = d.index(20)
+A = (20 + d[(i + 1) % 20] + d[i - 1]) / 3
+
+print("Alice" if A > 10.5 else "Tie" if A == 10.5 else "Bob")
