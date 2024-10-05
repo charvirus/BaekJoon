08ecd2c7ce0416fcb6ae79589ecc38ecd25b48a4
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-10-05 15:02:53 +0900

    [Bronze I] Title: ROT13, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+inp = input()
+for i in inp:
+    asc = 0
+    if i.isalpha():
+        asc = ord(i) + 13
+        if (ord(i) <= 90 < asc) or (ord(i) >= 97 and asc > 122):
+            print(chr(asc - 26), end='')
+        else:
+            print(chr(asc), end='')
+    else:
+        print(i, end="")
