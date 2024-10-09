03adc120ee1fa377e75f8c58694612bbf2de5f31
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-10-10 02:21:09 +0900

    [Silver V] Title: 벚꽃이 정보섬에 피어난 이유, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/17127. 벚꽃이 정보섬에 피어난 이유/벚꽃이 정보섬에 피어난 이유.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+t = 1
+
+def mul (a,b,arr):
+    mul = 1
+    for i in range(a,b):
+        mul *= arr[i]
+    return mul
+
+def sum(i,j,k,l,arr):
+    return mul(i,j,arr)+mul(j,k,arr)+mul(k,l,arr)+mul(l,len(arr),arr)
+
+for i in range(0,1):
+    for j in range(i+1,n-2):
+        for k in range(j+1,n-1):
+            for l in range(k+1,n):
+                t = max(sum(i,j,k,l,arr),t)
+
+print(t)
