c69f4b40bfa06344ad93cf4239a9892fd7058498
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-10-09 16:35:22 +0900

    [Bronze II] Title: 문어, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/21313. 문어/문어.py
+++ 백준/Bronze/21313. 문어/문어.py
@@ -1,9 +1,2 @@
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
-if n % 2 == 1:
-    print("1 2 "*int(n / 2)+"3")
-else:
-    print("1 2 "*int(n / 2))
+print(*[1,2] * (n // 2) + ([3] if n % 2 else []))
