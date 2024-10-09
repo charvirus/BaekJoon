831d176f7a21f58d39859d748b5ba1f2adc3391f
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-10-10 02:24:39 +0900

    [Silver V] Title: 벚꽃이 정보섬에 피어난 이유, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/17127. 벚꽃이 정보섬에 피어난 이유/벚꽃이 정보섬에 피어난 이유.py
+++ 백준/Silver/17127. 벚꽃이 정보섬에 피어난 이유/벚꽃이 정보섬에 피어난 이유.py
@@ -6,19 +6,18 @@ n = int(input())
 arr = list(map(int, input().split()))
 t = 1
 
-def mul (a,b,arr):
+def mul(a, b, arr):
     mul = 1
-    for i in range(a,b):
+    for i in range(a, b):
         mul *= arr[i]
     return mul
 
-def sum(i,j,k,l,arr):
-    return mul(i,j,arr)+mul(j,k,arr)+mul(k,l,arr)+mul(l,len(arr),arr)
+def sum(i, j, k, l, arr):
+    return mul(i, j, arr) + mul(j, k, arr) + mul(k, l, arr) + mul(l, len(arr), arr)
 
-for i in range(0,1):
-    for j in range(i+1,n-2):
-        for k in range(j+1,n-1):
-            for l in range(k+1,n):
-                t = max(sum(i,j,k,l,arr),t)
+for j in range(1, n - 2):
+    for k in range(j + 1, n - 1):
+        for l in range(k + 1, n):
+            t = max(sum(0, j, k, l, arr), t)
 
 print(t)
