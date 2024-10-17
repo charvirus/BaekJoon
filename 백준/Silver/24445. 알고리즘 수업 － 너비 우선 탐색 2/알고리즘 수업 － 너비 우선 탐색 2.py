4e9bcd77970ba7435a7557261268e18062214f58
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-10-17 14:38:10 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 892 ms, Memory: 60632 KB -BaekjoonHub

--- 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -2,21 +2,6 @@ import sys
 
 input = sys.stdin.readline
 
-
-def bfs(v):
-    global cnt
-    queue = [v]
-    vstd[v] = 1
-    while queue:
-        d = queue.pop(0)
-        smap[d].sort(reverse=True)
-        for i in smap[d]:
-            if not vstd[i]:
-                queue.append(i)
-                cnt += 1
-                vstd[i] = cnt
-
-
 N, M, V = map(int, input().split())
 smap = [[] for i in range(N + 1)]
 vstd = [0] * (N + 1)
@@ -27,6 +12,13 @@ for i in range(M):
     smap[a].append(b)
     smap[b].append(a)
 
-bfs(V)
-for i in vstd[1:]:
-    print(i)
+queue = [V]
+vstd[V] = 1
+while len(queue) != 0:
+    d = queue.pop(0)
+    for i in sorted(smap[d],reverse=True):
+        if not vstd[i]:
+            queue.append(i)
+            cnt += 1
+            vstd[i] = cnt
+print(*vstd[1:], sep='\n')
