0201e1506f0f6147076f07098e4d8c0d65df53d1
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-10-17 14:52:24 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 520 ms, Memory: 60992 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+def BFS(v):
+    global cnt
+    queue =  deque()
+    queue.append(v)
+    vstd[v] = 1
+    while queue:
+        d = queue.popleft()
+        for i in sorted(smap[d]):
+            if not vstd[i]:
+                queue.append(i)
+                cnt += 1
+                vstd[i] = cnt
+
+N, M, V = map(int, input().split())
+smap = [[] for i in range(N + 1)]
+vstd = [0] * (N + 1)
+cnt = 1
+
+for i in range(M):
+    a, b = map(int, input().split())
+    smap[a].append(b)
+    smap[b].append(a)
+
+BFS(V)
+print(*vstd[1:], sep='\n')
