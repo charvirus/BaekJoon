ce3315bc60a0f7e740c5874acb53bdb4e38e9a40
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-11-06 20:26:47 +0900

    [Silver V] Title: Toe Jumps, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/28463. Toe Jumps/Toe Jumps.py
+++ 백준/Silver/28463. Toe Jumps/Toe Jumps.py
@@ -2,9 +2,7 @@ import sys
 input = sys.stdin.readline
 
 D = input().strip()
-t = ""
-for i in range(2):
-    t += input().strip()
+t = input().strip() + input().strip()
 ans = [".OP.", "I..P", "O..P"]
 if D == "E":
     t = t[2] + t[0] + t[3] + t[1]
@@ -12,8 +10,6 @@ elif D == "W":
     t = t[1] + t[3] + t[0] + t[2]
 elif D == "N":
     t = t[3] + t[2] + t[1] + t[0]
-else:
-    t=t
 for i in range(3):
     if t == ans[i]:
         if i == 0:
