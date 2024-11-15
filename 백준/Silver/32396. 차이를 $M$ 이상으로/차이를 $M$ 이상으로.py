37af9be1f1a9b9495c71c304ce11fdfe4f8e674f
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-11-15 12:30:53 +0900

    [Silver III] Title: 차이를 $M$ 이상으로, Time: 608 ms, Memory: 155536 KB -BaekjoonHub

--- 백준/Silver/32396. 차이를 $M$ 이상으로/차이를 $M$ 이상으로.py
+++ 백준/Silver/32396. 차이를 $M$ 이상으로/차이를 $M$ 이상으로.py
@@ -6,6 +6,6 @@ seq = list(map(int, input().split()))
 cnt = 0
 for i in range(1, N):
     if M > abs(seq[i] - seq[i - 1]):
-        seq[i] += int(1e13)
+        seq[i] += 10000000000000
         cnt += 1
 print(cnt)
