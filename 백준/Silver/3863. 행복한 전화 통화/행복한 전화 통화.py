6238ee4cac1dc1fce4d4564d60d549205126d7c4
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-11-22 16:35:56 +0900

    [Silver V] Title: 행복한 전화 통화, Time: 1096 ms, Memory: 34188 KB -BaekjoonHub

--- 백준/Silver/3863. 행복한 전화 통화/행복한 전화 통화.py
+++ 백준/Silver/3863. 행복한 전화 통화/행복한 전화 통화.py
@@ -2,20 +2,21 @@ import sys
 
 input = sys.stdin.readline
 
-
 res = []
 while True:
     n, m = map(int, input().split())
     if n == 0 and m == 0:
         break
 
-    call = [[*map(int,input().split()[2:])] for _ in range(n)]
-    tap = [[*map(int,input().split())] for _ in range(m)]
+    call = [[*map(int, input().split()[2:])] for _ in range(n)]
+    tap = [[*map(int, input().split())] for _ in range(m)]
 
-    for i in range(m):
+    for a, b in tap:
         cnt = 0
-        for j in range(n):
-            if tap[i][0]<=call[j][0]<tap[i][0]+tap[i][1] or call[j][0]<=tap[i][0] <call[j][0]+call[j][1]:
+        for c, d in call:
+            # 통화 시작 시간 <= 도청 시작 시간 < 통화 끝나는 시간
+            # 도청 시작 시간 <= 통화 시작 시간 < 도청 끝나는 시간
+            if a <= c < a + b or c <= a < c + d:
                 cnt += 1
         res.append(cnt)
 print(*res, sep="\n")
