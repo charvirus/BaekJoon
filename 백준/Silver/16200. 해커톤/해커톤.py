4671b5fa48fa69084c45d7984a48b85bb91f4b10
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-11-23 16:12:53 +0900

    [Silver IV] Title: 해커톤, Time: 72 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/16200. 해커톤/해커톤.py
+++ 백준/Silver/16200. 해커톤/해커톤.py
@@ -1,12 +1,11 @@
 import sys
-
 input = sys.stdin.readline
 
 n = int(input())
 x = sorted(list(map(int, input().split())))
-cnt = 0
+c = 0
 i = 0
-while i<n:
+while i < n:
     i += x[i]
-    cnt += 1
-print(cnt)
+    c += 1
+print(c)
