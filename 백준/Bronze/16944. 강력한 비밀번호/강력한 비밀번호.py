ac4c8c1f0d3487be6290a62261445862bae23fa9
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-11-27 13:19:22 +0900

    [Bronze II] Title: 강력한 비밀번호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/16944. 강력한 비밀번호/강력한 비밀번호.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+inp = input().strip()
+tmp = [1, 1, 1, 1]
+for i in inp:
+    if ord(i) >= 48 and ord(i) <= 57:
+        tmp[0] = 0
+    elif ord(i) >= 65 and ord(i) <= 91:
+        tmp[1] = 0
+    elif ord(i) >= 97 and ord(i) <= 123:
+        tmp[2] = 0
+    elif i in "@#$%^&*()-+":
+        tmp[3] = 0
+n += sum(tmp)
+if n > 6:
+    print(sum(tmp))
+else:
+    print(sum(tmp) + 6 - n)
