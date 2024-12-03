9cd5446fedf4af3ac505929209bc361d515af5ff
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-12-03 19:40:31 +0900

    [Bronze I] Title: 2022년이 아름다웠던 이유, Time: 256 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27065. 2022년이 아름다웠던 이유/2022년이 아름다웠던 이유.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def fac(x):
+    facs = []
+    for i in range(1, x + 1):
+        if x % i == 0:
+            facs.append(i)
+    return facs
+
+
+for i in range(int(input())):
+    inp = int(input())
+    facts = fac(inp)
+    flag = True
+    if sum(fac(facts[-1])) - facts[-1] <= facts[-1]:
+        print("BOJ 2022")
+        flag = False
+    else:
+        for j in range(len(facts) - 1):
+            if sum(fac(facts[j])) - facts[j] > facts[j]:
+                print('BOJ 2022')
+                flag = False
+                break
+        if flag:
+            print('Good Bye')
