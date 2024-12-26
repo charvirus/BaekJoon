0eb9395854895209c017cc198472d1263f480feb
Brandon J <43872889+charvirus@users.noreply.github.com>
2024-12-26 11:48:00 +0900

    [Silver V] Title: 숫자 빈도수, Time: 60 ms, Memory: 38792 KB -BaekjoonHub

--- 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.py
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.py
@@ -1,10 +1,3 @@
-import sys
-
-input = sys.stdin.readline
-
 n, m = map(int, input().split())
-cnt = 0
-for i in range(1,n + 1):
-    w = str(i)
-    cnt += w.count(str(m))
-print(cnt)
+r = ''.join([str(i) for i in range(1, n+1)])
+print(r.count(str(m)))
