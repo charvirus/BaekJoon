6d5da81c7bb7f7a2bee31df0fdb1b09c8353e198
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-01-07 20:58:33 +0900

    [Silver IV] Title: 국영수, Time: 364 ms, Memory: 83912 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,9 +1,9 @@
-
+import sys
+input = sys.stdin.readline
 n = int(input())
 std = []
 for _ in range(n):
     std.append(input().split())
-
 std.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 for student in std:
