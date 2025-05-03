0e2e2258567f5b599893a822cec91813bca843a0
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-05-03 12:07:11 +0900

    [Silver II] Title: 과일 탕후루, Time: 188 ms, Memory: 35588 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -1,26 +1,21 @@
-from collections import defaultdict as defd
 import sys
-from itertools import count
 
 input = sys.stdin.readline
 
 n = int(input())
 lst = list(map(int, input().split()))
 
-l, r, cnt = 0, 0, 0
-info = defd(int)
+k, low, cnt = 0, 0, [0] * 10
 ans = 0
 
-while r < n:
-    if info[lst[r]] == 0:
-        cnt += 1
-    info[lst[r]] += 1
-
-    while cnt > 2:
-        info[lst[l]] -= 1
-        if info[lst[l]] == 0:
-            cnt -= 1
-        l += 1
-    ans = max(ans, r - l + 1)
-    r += 1
+for i in range(n):
+    cnt[lst[i]] += 1
+    if cnt[lst[i]] == 1:
+        k += 1
+    while k > 2:
+        cnt[lst[low]] -= 1
+        if cnt[lst[low]] == 0:
+            k -= 1
+        low += 1
+    ans = max(ans, i - low + 1)
 print(ans)
