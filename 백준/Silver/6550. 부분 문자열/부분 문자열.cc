2811ad49e94b2cd0e0b41918b379ab3d1769c1a2
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-07-04 12:53:10 +0900

    [Silver V] Title: 부분 문자열, Time: 4 ms, Memory: 2272 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.cc
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.cc
@@ -5,18 +5,13 @@ using namespace std;
 int main() {
     string a, b;
     while (cin >> a >> b) {
-        bool flag = false;
         int idx = 0;
         for (int i = 0; i < b.length(); i++) {
             if (a[idx] == b[i]) {
                 idx++;
             }
-            if (idx == a.length()) {
-                flag = true;
-                break;
-            }
         }
-        if (flag) {
+        if (idx == a.length()) {
             cout << "Yes" << '\n';
         } else {
             cout << "No" << '\n';
