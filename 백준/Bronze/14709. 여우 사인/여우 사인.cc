dd53bca537ceacd2f3200db31ff287e883fc128b
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-07-09 11:23:45 +0900

    [Bronze I] Title: 여우 사인, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/14709. 여우 사인/여우 사인.cc
+++ 백준/Bronze/14709. 여우 사인/여우 사인.cc
@@ -1,25 +1,22 @@
-#include <iostream>
+#include <stdio.h>
 using namespace std;
 bool arr[5];
 int n;
-
 int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0);
-    cout.tie(0);
-    cin >> n;
-    if (n != 3) cout << "Woof-meow-tweet-squeek";
+    scanf("%d", &n);
+    if (n != 3)printf("Woof-meow-tweet-squeek");
     else {
         while (n--) {
             int a, b;
-            cin >> a >> b;
+            scanf("%d %d", &a, &b);
             a -= 1, b -= 1;
             arr[a] = arr[b] = true;
         }
 
-        if (arr[1] == false && arr[4] == false) {
-            cout << "Wa-pa-pa-pa-pa-pa-pow!";
+        if (!arr[1] && !arr[4]) {
+            printf("Wa-pa-pa-pa-pa-pa-pow!");
         } else {
-            cout << "Woof-meow-tweet-squeek";
+            printf("Woof-meow-tweet-squeek");
         }
     }
 }
