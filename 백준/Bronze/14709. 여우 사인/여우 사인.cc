2d6e162a572055405227e1620628046b512c7a74
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-07-09 11:24:44 +0900

    [Bronze I] Title: 여우 사인, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/14709. 여우 사인/여우 사인.cc
+++ 백준/Bronze/14709. 여우 사인/여우 사인.cc
@@ -1,18 +1,16 @@
 #include <stdio.h>
-using namespace std;
 bool arr[5];
-int n;
+
 int main() {
+    int n, a, b;
     scanf("%d", &n);
     if (n != 3)printf("Woof-meow-tweet-squeek");
     else {
         while (n--) {
-            int a, b;
             scanf("%d %d", &a, &b);
             a -= 1, b -= 1;
             arr[a] = arr[b] = true;
         }
-
         if (!arr[1] && !arr[4]) {
             printf("Wa-pa-pa-pa-pa-pa-pow!");
         } else {
