42c28368147ab265c091dbe384db97875ee79044
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-07-14 16:09:35 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int dp[50005];
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = dp[i - 1] + 1;
+        for (int j = 1; j * j <= i; j++)
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+    }
+    cout << dp[n] << endl;
+}
