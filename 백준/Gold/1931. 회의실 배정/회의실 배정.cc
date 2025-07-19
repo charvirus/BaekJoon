3dc875c7b585771f0ce9109df4c2cd8b61b44333
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-07-19 23:50:24 +0900

    [Gold V] Title: 회의실 배정, Time: 28 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(0);
+    int n,cnt=1;
+    vector<pair<int, int> > list;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int s, e;
+        cin >> e >> s;
+        list.push_back({s, e});
+    }
+    sort(list.begin(), list.end());
+    int end = list[0].first;
+    for (int i = 1; i < n; i++) {
+        if (end <= list[i].second) {
+            end = list[i].first;
+            cnt++;
+        }
+    }
+    cout << cnt << '\n';
+}
