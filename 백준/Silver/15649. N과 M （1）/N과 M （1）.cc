9558f00d5001ccd13b7cf437f58ea82ef362d7e7
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-08-18 14:55:04 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[9] = {0,};
+bool vst[9]={false,};
+
+void dfs(int cnt) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!vst[i]) {
+            vst[i] = true;
+            arr[cnt] = i;
+            dfs(cnt+1);
+            vst[i] = false;
+        }
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> n >> m;
+    dfs(0);
+}
