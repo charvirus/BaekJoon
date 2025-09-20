d01ecb42399887fe83ae3bd5e91d26f4985ec2a0
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-09-21 00:44:38 +0900

    [Bronze V] Title: Above, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34362. Above/Above.cc
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    double n;
+    cin >> n;
+    cout.setf(ios::showpoint);
+    cout<< n - 0.3;
+}
