0898ffafe5f86b296283dae2b9ab2dd065ac56bf
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-09-25 23:18:16 +0900

    [Silver V] Title: 팩트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6463. 팩트/팩트.cc
+++ 백준/Silver/6463. 팩트/팩트.cc
@@ -9,12 +9,11 @@ int main(void) {
     while (cin >> a) {
         long fact = 1;
         for (int i = 1; i <= a; ++i) {
-            long mul = i;
-            while (mul % 5 == 0) {
-                mul /= 5;
-                fact /= 2;
+            fact *= i;
+            while (fact % 10==0) {
+                fact /= 10;
             }
-            fact = (fact%100000)*mul;
+            fact %= 1000000000;
         }
         cout.width(5);
         cout << a << " -> " << (fact % 10) << "\n";
