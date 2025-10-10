28ab2e4cb7e4f092daa29e2047fd48149685a979
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-10-11 00:30:25 +0900

    [Bronze IV] Title: 파일 옮기기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11943. 파일 옮기기/파일 옮기기.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    if (a + d <= b + c)
+        cout<<a + d;
+    else
+        cout<<b + c;
+}
