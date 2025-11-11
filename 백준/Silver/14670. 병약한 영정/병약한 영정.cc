9cefeaa2c7b6bef04b20e9a77f0e074e24b9960e
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-11-11 23:32:38 +0900

    [Silver IV] Title: 병약한 영정, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14670. 병약한 영정/병약한 영정.cc
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <vector>
+using namespace std;
+int n, m;
+int a[101];
+vector<int> ans;
+
+int main(void) {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    memset(a, 0xff, sizeof(a));
+    cin >> n;
+    while (n--) {
+        int x, y;
+        cin >> x >> y;
+        a[x] = y;
+    }
+    cin >> n;
+    while (n--) {
+        bool chk = 1;
+        ans.clear();
+        cin >> m;
+        while (m--) {
+            int x;
+            cin >> x;
+            if (a[x] < 0)chk = 0;
+            else ans.push_back(a[x]);
+        }
+        if (chk) {
+            for (auto &x: ans) cout << x << " ";
+
+            cout << "\n";
+        } else
+            cout << "YOU DIED\n";
+
+    }
+}
