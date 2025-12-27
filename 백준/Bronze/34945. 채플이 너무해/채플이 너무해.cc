8767ba984fece735fc3acf2b220083d366d968cd
Brandon J <43872889+charvirus@users.noreply.github.com>
2025-12-27 16:38:59 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    int n;
+    cin>>n;
+    if (n>=6) {
+        cout<<"Success!";
+    }else {
+        cout<<"Oh My God!";
+    }
+}
