9291319ea01ad7d624c9e550525808f97164618f
Brandon J <43872889+charvirus@users.noreply.github.com>
2026-01-12 22:00:17 +0900

    [Silver V] Title: 레몬 게임, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/34248. 레몬 게임/레몬 게임.cc
+++ 백준/Silver/34248. 레몬 게임/레몬 게임.cc
@@ -1,61 +1,15 @@
-#include <algorithm>
-#include <cmath>
 #include <iostream>
-
 using namespace std;
+int n, one, two, in;
 
 int main(void) {
     ios::sync_with_stdio(false), cin.tie(0);
-    int n;
     cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    while (n--) {
+        cin >> in;
+        if (in == 2) two++;
+        else one++;
     }
-
-    for (int i = 0; i < n; i++) {
-        int st = i, en, sum = 0;
-        bool flag = false;
-        for (int j = i; j < n; j++) {
-            sum += arr[j];
-            if (sum == 3) {
-                en = j;
-                flag = true;
-                break;
-            } else if (sum > 3) {
-                break;
-            }
-        }
-        if (flag) {
-            for (int j = st; j <= en; j++) {
-                arr[j] = 0;
-            }
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        int st = i, en, sum = 0;
-        bool flag = false;
-        for (int j = i; j < n; j++) {
-            sum += arr[j];
-            if (sum == 3) {
-                en = j;
-                flag = true;
-                break;
-            } else if (sum > 3) {
-                break;
-            }
-        }
-        if (flag) {
-            for (int j = st; j <= en; j++) {
-                arr[j] = 0;
-            }
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == 1 || arr[i] == 2) {
-            cout << "No";
-            return 0;
-        }
-    }
-    cout << "Yes";
+    if (two <= one && (two * 2 + one) % 3 == 0) cout << "Yes";
+    else cout << "No";
 }
