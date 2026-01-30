0a50e64ba339de20119d3a006ab9e777ca8e56a8
Brandon J <43872889+charvirus@users.noreply.github.com>
2026-01-30 14:34:57 +0900

    [Bronze I] Title: 테니스 시합, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Bronze/2183. 테니스 시합/테니스 시합.cc
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string s,inp;
+
+int main(void) {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> s>>inp;
+    cout<<inp.back();
+}
