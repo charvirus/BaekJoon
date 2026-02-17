78d1ede4ada8a446871a5cfa889add06adedb6fc
Brandon J <43872889+charvirus@users.noreply.github.com>
2026-02-17 22:53:15 +0900

    [Bronze IV] Title: Contest Timing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5928. Contest Timing/Contest Timing.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int d,h,m;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+    cin >> d >> h >> m;
+    int pivot = 11 + 11 * 60 + 11 * 60 * 24;
+    int ans = m + h * 60 + d * 60 * 24 - pivot;
+    if(ans < 0 ) cout << -1 <<'\n';
+    else cout << ans <<'\n';
+}
