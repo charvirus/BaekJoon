3c9f9d38819c2bdd093ffe92e757bbdab9f85171
Brandon J <43872889+charvirus@users.noreply.github.com>
2026-03-04 00:17:23 +0900

    [Silver V] Title: SSHS 문자열, Time: 8 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/34953. SSHS 문자열/SSHS 문자열.cc
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main(void) {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cout << "SSH"[i % 3];
+    }
+
+}
