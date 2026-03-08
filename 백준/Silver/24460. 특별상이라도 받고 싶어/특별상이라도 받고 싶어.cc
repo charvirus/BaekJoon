7e810f54b15709c2fece6b0a94692c78929b2a00
Brandon J <43872889+charvirus@users.noreply.github.com>
2026-03-08 11:02:39 +0900

    [Silver III] Title: 특별상이라도 받고 싶어, Time: 136 ms, Memory: 392644 KB -BaekjoonHub

---
+++ 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int n, arr[10000][10000];
+
+int req(int x, int y, int n) {
+    int ans[4] = {0,};
+    if (n >= 2) {
+        ans[0] = req(x, y, n / 2);
+        ans[1] = req(x + n / 2, y, n / 2);
+        ans[2] = req(x, y + n / 2, n / 2);
+        ans[3] = req(x + n / 2, y + n / 2, n / 2);
+        sort(ans,ans+4);
+        return ans[1];
+    }else {
+        return arr[x][y];
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    cout << req(0, 0, n);
+}
