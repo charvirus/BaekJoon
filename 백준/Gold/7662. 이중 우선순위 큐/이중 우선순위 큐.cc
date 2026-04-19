972f660dc11c160f67f2bda42d854d077a9a4643
Brandon J <43872889+charvirus@users.noreply.github.com>
2026-04-19 10:52:25 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1352 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,63 +1,39 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <vector>
+#include <set>
 using namespace std;
-
-
-priority_queue<int, vector<int>, greater<int> > minq;
-priority_queue<int, vector<int>, less<int> > maxq;
-map<int, int> cnt;
-
-void init() {
-    while (!minq.empty()) {
-        minq.pop();
-    }
-    while (!maxq.empty()) {
-        maxq.pop();
-    }
-    cnt.clear();
-}
-
-void update() {
-    while (!minq.empty() && !cnt[minq.top()]) {
-        minq.pop();
-    }
-    while (!maxq.empty() && !cnt[maxq.top()]) {
-        maxq.pop();
-    }
-}
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(NULL);
-    int n, inp,cinp;
-    char cmd;
+    int n, inp;
     cin >> n;
     while (n--) {
-        init();
-        cin >> inp;
-        for (int i = 0; i < inp; i++) {
-            cin >> cmd >> cinp;
-            if (cmd == 'D') {
-                if (cinp == -1 && !minq.empty()) {
-                    cnt[minq.top()]--;
-                    minq.pop();
-                } else if (cinp == 1 && !maxq.empty()) {
-                    cnt[maxq.top()]--;
-                    maxq.pop();
+        multiset<int> s;
+        int cn;
+        cin >> cn;
+        for (int i = 0; i < cn; i++) {
+            char cmd;
+            cin >> cmd;
+            if (cmd == 'I') {
+                cin >> inp;
+                s.insert(inp);
+            } else if (cmd == 'D') {
+                cin >> inp;
+                if (!s.empty() && inp == -1) {
+                    s.erase(s.begin());
+                } else if (!s.empty() && inp == 1) {
+                    auto it = s.end();
+                    it--;
+                    s.erase(it);
                 }
-                update();
-            } else if (cmd == 'I') {
-                maxq.push(cinp);
-                minq.push(cinp);
-                cnt[cinp]++;
             }
         }
-        if (minq.empty() || maxq.empty()) {
+        if (s.empty()) {
             cout << "EMPTY" << '\n';
         } else {
-            cout << maxq.top() << ' ' << minq.top() << '\n';
+            auto it = s.end();
+            it--;
+            cout << *it << ' ' << *s.begin() << '\n';
         }
     }
 }
